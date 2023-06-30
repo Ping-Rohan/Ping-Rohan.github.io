63e163226ea9b8057e415bed474e217b2d0a91ed
Rohan Tiwari <ping.techyrohan@gmail.com>
2023-06-30 13:38:28 +0545

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: "##e4e4e7",
+          padding: "10px 20px",
+          borderRadius: "10px",
+          color: "#18181b",
+        }}
+      >
+        Website Under Construction
+      </h1>
+    </div>
+  );
+}
+
+export default App;
